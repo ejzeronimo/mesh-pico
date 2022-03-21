20d7b89b1368802cbda51d56dab65f313248aa7a
Ethan Zeronik <ejzeronik@gmail.com>
2022-03-21 17:39:54 -0400

    just need to get the flash working

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,100 @@
+// pico SDK stuff
+#include "hardware/flash.h"
 #include "pico/stdlib.h"
+
+// c libs
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
 
+// library I found for JSON parsing and other goodies
+#include <mjson.h>
+
+// stuff for flash
+#define FLASH_TARGET_OFFSET (128 * 1024)
+const uint8_t *flash_target_contents = (const uint8_t *)(XIP_BASE + FLASH_TARGET_OFFSET);
+char *id;
+
+// NOTE: defined for RPC engine (provided by mjson)
+static int sender(const char *frame, int frame_len, void *privdata) {
+
+    for (size_t i = 0; i < frame_len; i++) {
+        uart_putc(uart1, frame[i]);
+    }
+
+    return 0;
+}
+
+// NOTE: should take a param and set our serial number and the flash
+static void setChassisSerialNumber(struct jsonrpc_request *r) {
+    bool success = false;
+
+    // erase our old data here
+    flash_range_erase(FLASH_TARGET_OFFSET, FLASH_SECTOR_SIZE);
+
+    // get our params
+    char temp[64];
+    mjson_get_string(r->params, r->params_len, "$[0]", temp, sizeof(temp));
+
+    uart_puts(uart1, temp);
+
+    flash_range_program(FLASH_TARGET_OFFSET, (const uint8_t *)temp, FLASH_PAGE_SIZE);
+
+    id = (char *)flash_target_contents;
+
+    success = true;
+
+    // do something
+    jsonrpc_return_success(r, "%s", success ? "true" : "false");
+}
+
+// NOTE: should return our stored serial number
+static void getChassisSerialNumber(struct jsonrpc_request *r) {
+    std::string temp = std::string(id);
+    // just return or string
+    jsonrpc_return_success(r, "%s", ("\"" + temp + "\""));
+}
+
+// NOTE: the main loop
 int main(void) {
-    // Initialise UART 0
-    uart_init(uart1, 115200);
+    // init RPC stuff
+    jsonrpc_init(NULL, NULL);
+    jsonrpc_export("setChassisSerialNumber", setChassisSerialNumber);
+    jsonrpc_export("getChassisSerialNumber", getChassisSerialNumber);
 
-    // set the gpio
+    // init UART 1 (not the USB port)
+    uart_init(uart1, 115200);
     gpio_set_function(4, GPIO_FUNC_UART);
     gpio_set_function(5, GPIO_FUNC_UART);
 
+    // basic LED stuff
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     while (true) {
-        //TODO:
-        /*
-            We want a few things to happen
-            get the serial number stored in flash
-            store it here
-            when we get a char string from serial port desipher it to JSON
+        char buf[512];
+        size_t len;
 
-            depending on if it is a GET/SET request do the following
+        if (uart_is_readable(uart1) > 0) {
+            gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
-            get the serial number and return it
-        */
-        uart_puts(uart1, "\nBreak World\n");
+            len = 0;
 
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(500);
+            // get the string
+            while (uart_is_readable_within_us(uart1, 1000) > 0) {
+                char temp = uart_getc(uart1);
+
+                if (temp == '\n') {
+                    break;
+                }
+
+                buf[len] = temp;
+                len++;
+            }
+
+            jsonrpc_process(buf, len, sender, NULL, NULL);
+        }
+
         gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(500);
     }
 }
